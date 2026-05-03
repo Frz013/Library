912a7ae73ce204e6becd124192ae9f5bfa9e0b32
Frz013 <ferp130508@gmail.com>
2026-05-03 14:33:57 +0700

    new file:   .gitignore 	modified:   __pycache__/main.cpython-310.pyc 	modified:   services/__pycache__/db.cpython-310.pyc 	modified:   services/db.py 	modified:   tools/__pycache__/book.cpython-310.pyc 	modified:   tools/book.py

--- services/db.py
+++ services/db.py
@@ -1,5 +1,7 @@
 
 import mysql.connector
+import random
+import string
 
 db = mysql.connector.connect(
     host = 'localhost',
@@ -7,6 +9,18 @@ db = mysql.connector.connect(
     password = '',
     database = 'library'
 )
+def create_kode_buku(kode_buku_cek):
+    cur = db.cursor()
+    is_kode_buku_exists ="SELECT EXISTS(SELECT 1 FROM tbl_book WHERE kode_buku = %s)"
+    cur.execute(is_kode_buku_exists, (kode_buku_cek))
+
+    result = cur.fetchone()
+    while True:
+        if result[0] == 0:
+            kode_buku = ''.join(random.choice(string.ascii_letters.upper()) for i in range(6))
+            return kode_buku
+        elif result[0] == 1:
+            continue
 
 def insert_item(kode_buku, judul_buku, penulis_buku, tahun_terbit_buku, sinopsis_buku, tgl_update):
     cur = db.cursor()
@@ -26,3 +40,31 @@ def fetch_item_by_id(id):
     cur = db.cursor()
     cur.execute("SELECT * FROM tbl_book WHERE id=%s", (id,))
     return cur.fetchone()
+
+def update_item(opsi_update, kode_buku):
+    cur = db.cursor()
+    is_kode_buku_exists ="SELECT EXISTS(SELECT 1 FROM tbl_book WHERE kode_buku = %s)"
+    cur.execute(is_kode_buku_exists, (kode_buku,))
+
+    result = cur.fetchone()
+    if result[0] == 0:
+        print("kode buku tidak ditemukan")
+
+    elif result[0] == 1:
+        
+        if opsi_update == 1:
+            print("Update Data Judul")
+            data_baru = str(input("Masukan judul yang baru: "))
+            update_data = "UPDATE tbl_book SET judul_buku =%s WHERE kode_buku =%s"
+            input_data_baru = (data_baru, kode_buku)
+            cur.execute(update_data, input_data_baru)
+            db.commit()
+            print("judul berhasil di update")
+
+
+        elif opsi_update == 2:
+            return
+        elif opsi_update == 3:
+            return
+        elif opsi_update == 4:
+            return
--- tools/book.py
+++ tools/book.py
@@ -1,5 +1,6 @@
 from services import db
 from datetime import datetime as dt
+from services.db import create_kode_buku
 import random
 import string
 import os
@@ -15,7 +16,7 @@ def start():
         elif opsi == 2:
             add_data()
         elif opsi == 3:
-            pass
+            update_data()
         elif opsi == 4:
             pass
         elif opsi == 5:
@@ -25,7 +26,8 @@ def start():
 def add_data():
     os.system('cls')
     print("Tolong masukan data yang diminta: ")
-    kode_buku = ''.join(random.choice(string.ascii_letters.upper()) for i in range(6))
+
+    kode_buku = create_kode_buku()
     judul_buku = str(input("Masukan Judul Buku: "))
     penulis_buku = str(input("Masukan Nama Penulis Buku: "))
     tahun_terbit_buku = str(input("Masukan Tahun Terbit Buku: "))
@@ -39,12 +41,15 @@ def read_data():
     books = db.fetch_item()
     os.system('cls')
     for book in books:
+        nomor_list = 1
         id_buku = book[0]
         kode_buku = book[1]
         judul_buku = book[2]
         print(f'''
-ID: {id_buku} | KODE: {kode_buku} | JUDUL: {judul_buku}
+NO.: {nomor_list} | ID: {id_buku} | KODE: {kode_buku} | JUDUL: {judul_buku}
 ''')  
+        nomor_list += 1
+
     opsi = int(input("Pilih opsi berikut:\n1. Detail buku\n2. Kembali ke menu\nPilih opsi: "))
     if opsi == 1:
         id_detail = int(input("Pilih ID buku yang ingin dilihat detailnya: "))
@@ -70,3 +75,13 @@ UPDATE DATA TERAKHIR PADA: {tgl_update}
         input("tekan enter untuk kembali")
     elif opsi == 2:
         pass
+
+def update_data():
+    os.system("cls")
+    kode_buku = str(input("masukan kode buku yang ingin di update (kembali ke menu untuk melihat kode buku): "))
+    if kode_buku == "back":
+        pass
+
+    opsi_update = int(input("pilih opsi yang ingin diupdate:\n1. judul buku\n2. penulis buku\n3. tahun terbit buku\n4. sinopsis buku\n pilih opsi: "))
+    db.update_item(opsi_update, kode_buku)
+
